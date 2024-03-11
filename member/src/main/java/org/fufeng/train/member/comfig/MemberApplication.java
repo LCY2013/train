5673233b8e088b6a2160e5374d5290d596a3093a
fufeng <15281205719@163.com>
2024-03-11 21:51:47 +0800

    feat: logback

--- member/src/main/java/org/fufeng/train/member/comfig/MemberApplication.java
+++ member/src/main/java/org/fufeng/train/member/comfig/MemberApplication.java
@@ -1,13 +1,22 @@
 package org.fufeng.train.member.comfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.fufeng.train.member"})
 public class MemberApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MemberApplication.class).run(args);
+        final SpringApplication app = new SpringApplication(MemberApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("测试地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 }
