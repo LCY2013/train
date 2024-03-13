c97e00e1d2ad6ab1986a7abadc0b41dbdb4b6532
fufeng <15281205719@163.com>
2024-03-13 21:30:02 +0800

    feat: add gateway project

---
+++ gateway/src/main/java/org/fufeng/train/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package org.fufeng.train.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * 网关项目
+ * 开启日志：-Dreactor.netty.http.server.accessLogEnabled=true
+ *
+ * @author fufeng
+ * @Date 2024-03-13 21:07
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+    public static void main(String[] args) {
+        final SpringApplication app = new SpringApplication(GatewayApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("网关：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
