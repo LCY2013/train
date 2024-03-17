1d321d3a3d4d2c0407a2ef0cab4ddbbc3ca4857c
fufeng <15281205719@163.com>
2024-03-17 20:43:01 +0800

    feat: nacos

--- member/src/main/java/org/fufeng/train/member/controller/TestController.java
+++ member/src/main/java/org/fufeng/train/member/controller/TestController.java
@@ -1,13 +1,22 @@
 package org.fufeng.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.nacos}")
+    private String testNacos;
+
+    @Autowired
+    Environment environment;
 
     @GetMapping(value = "/hello")
     public String hello() {
-        return "Hello World!";
+        String port = environment.getProperty("local.server.port");
+        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 }
